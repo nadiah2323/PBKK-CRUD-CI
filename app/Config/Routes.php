eaba6526a8e203921f0a5cebf5e8151c34a7ce93
nadiah2323 <114987104+nadiah2323@users.noreply.github.com>
2023-12-02 23:32:00 +0700

    Add files via upload

---
+++ app/Config/Routes.php
@@ -0,0 +1,23 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+// Halo
+$routes->get('halo', 'Halo::index');
+// Home Barang
+$routes->get('/', 'Barang::index');
+$routes->get('barang', 'Barang::index');
+// Halaman Tambah
+$routes->get('barang/tambah', 'Barang::tambah');
+// Halaman Edit
+$routes->get('barang/edit/(:any)', 'Barang::edit/$1');
+// Proses CRUD
+// Insert
+$routes->post('barang/add', 'Barang::add');
+// Update
+$routes->post('barang/update', 'Barang::update');
+// Hapus
+$routes->get('barang/hapus/(:any)', 'Barang::hapus/$1');
